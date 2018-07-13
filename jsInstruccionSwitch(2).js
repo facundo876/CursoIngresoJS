48b5dbc09d4ea29ac7407538cc5827136a9704c5
facundo876 <40859125+facundo876@users.noreply.github.com>
2018-07-13 11:58:53 -0300

    info tps

---
+++ jsInstruccionSwitch(2).js
@@ -0,0 +1,18 @@
+function mostrar()
+{
+//tomo la edad  
+var mesDelAño = document.getElementById('mes').value;
+
+alert (mesDelAño);
+
+
+	/*1 a 10 inico,
+	 1 a 3 tp,
+	 1 a 3 recuperatorio, 
+	 1 a 10 if 
+	 1 a 6 switch
+	 4 a 5 recuper,
+	 4 del tp con (if+if, if+switch, switch+if, switch+switch )
+
+
+}//FIN DE LA FUNCIÓN
