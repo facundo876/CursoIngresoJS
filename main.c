6f0c90cd8d35dd3d90c0097ca58fe09331efb1e5
facundo876 <40859125+facundo876@users.noreply.github.com>
2019-03-20 12:42:01 -0300

    clase 2 laboratorio + swich

--- main.c
+++ main.c
@@ -3,6 +3,30 @@
 
 int main()
 {
-    //printf("Hello world!\n");
+    char valor= 'b';
+    switch(valor){
+        case '1':
+        case '2':
+        case '3':
+        case '4':
+        case '5':
+        case '6':
+            printf("a dormir");
+            break;
+        case '7':
+            printf("buenos dias");
+            break;
+        case '8':
+            printf("cualquier cosa");
+            break;
+        case 'a':
+                printf("10");
+                break;
+        default:
+                    printf("error");
+                    break;
+    }
+
+
     return 0;
 }
