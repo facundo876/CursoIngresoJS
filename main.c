eb83ee19938f957d2999dd08a73af5ddabc4189b
facundo876 <40859125+facundo876@users.noreply.github.com>
2019-03-20 12:44:44 -0300

    Add files via upload

--- main.c
+++ main.c
@@ -1,32 +1,73 @@
 #include <stdio.h>
 #include <stdlib.h>
+// ENTREGA DE T.PRACTICO Nº1 ANTES DE 5/4/19
 
+/*
+float result;
+iny n1, n2;
+n1=5;
+n2=2;
+resul=(float)n1/n2; crea un resultado en flotante se llama castear
+castea solo an n1
+*/
 int main()
 {
-    char valor= 'b';
-    switch(valor){
-        case '1':
-        case '2':
-        case '3':
-        case '4':
-        case '5':
-        case '6':
-            printf("a dormir");
-            break;
-        case '7':
-            printf("buenos dias");
-            break;
-        case '8':
-            printf("cualquier cosa");
-            break;
-        case 'a':
-                printf("10");
-                break;
-        default:
-                    printf("error");
-                    break;
-    }
+   //int numero= 0;
+    //while
+    /*while (numero<10){ //hasta que numero deje de ser menor a 10
 
+        numero++;
+        printf("%d ,", numero);
+    }*/
+
+    //do whlie se usa cuando no sabes cuantas veses vas a repertir
+    /*do{
+    numero++;
+    printf("%d ,", numero);
+    }while(numero<10);*/
+
+    //for
+
+    /*for(numero=0; numero<10; numero++){
+
+        printf("%d\n", (numero+1));
+    }
+    printf("%d", numero);*/
+
+    //Ejercicio ____________________________--_____________
+
+    /*int i, numero, promedio=0, maximo, minimo, flag=0;//
+
+    for(i=0; i<5; i++){
+        printf("Ingresar numero [%d] : ", i);
+        scanf("%d", &numero);
+
+        promedio=promedio+numero;
+    }
+    printf("Promedio : %d", (promedio/i));*/
+    //#ctype.h bibloteca
+    int valro, min, max, flag=0;
+    char rta;
+    do{
+        if(flag==0 || numero>max){//consepto de bandera
+
+            max=numero;
+        }
+        if(flag==0 || numero<min){
+
+            min=numero;
+            flag=1;
+        }
+      //validar
+      do{
+        printf("continuar [n/s]?");
+        fflush(stdin);
+        scanf("%d", &rta);
+        rta =topper(rta);
+
+      }while(rta!='s'&& rta!='n');
+    }while(rta=='s')
 
     return 0;
 }
+//limpiar fflush( stdin );
